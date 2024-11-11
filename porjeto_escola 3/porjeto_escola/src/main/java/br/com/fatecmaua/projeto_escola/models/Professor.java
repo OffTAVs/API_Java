acc6d175a4a7df8697229456fa2a6a149c5339c9
Otávio Barbosa Ferreira <106000641+OffTAVs@users.noreply.github.com>
2024-11-11 19:44:05 -0300

    Add files via upload

---
+++ porjeto_escola 3/porjeto_escola/src/main/java/br/com/fatecmaua/projeto_escola/models/Professor.java
@@ -0,0 +1,108 @@
+package br.com.fatecmaua.projeto_escola.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "professor")
+public class Professor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	@JoinColumn(name = "id_turma")
+	private Turma turma;
+	private String nome;
+	private String RG;
+	private String email;
+	private String senha;
+
+	public Professor(Long id, Turma turma, String nome, String RG, String email, String senha) {
+		this.id = id;
+		this.turma = turma;
+		this.nome = nome;
+		this.RG = RG;
+		this.email = email;
+		this.senha = senha;
+	}
+
+
+public Professor() {
+
+}
+
+
+public Long getId() {
+	return id;
+}
+
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+
+public Turma getTurma() {
+	return turma;
+}
+
+
+public void setTurma(Turma turma) {
+	this.turma = turma;
+}
+
+
+public String getNome() {
+	return nome;
+}
+
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+
+public String getRG() {
+	return RG;
+}
+
+
+public void setRG(String rG) {
+	RG = rG;
+}
+
+
+public String getEmail() {
+	return email;
+}
+
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+
+public String getSenha() {
+	return senha;
+}
+
+
+public void setSenha(String senha) {
+	this.senha = senha;
+}
+
+
+
+
+
+
+
+
+}
---
+++ porjeto_escola 3/porjeto_escola/src/main/java/br/com/fatecmaua/projeto_escola/swagger/ConfigSwagger.java
@@ -0,0 +1,18 @@
+package br.com.fatecmaua.projeto_escola.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class ConfigSwagger {
+	@Bean
+	public OpenAPI customAPI() {
+		return new OpenAPI().info(
+				new Info().title("Projeto Música")
+				.description("Este é um projeto sobre música e artistas musicais").version("1.0.0"));
+	}
+
+}
