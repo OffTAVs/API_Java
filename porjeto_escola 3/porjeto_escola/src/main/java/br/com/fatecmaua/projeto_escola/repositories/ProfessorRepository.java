d974673c0a35bbcf44ce5adcba43f40f1ff216f0
Otávio Barbosa Ferreira <106000641+OffTAVs@users.noreply.github.com>
2024-11-18 20:02:51 -0300

    Delete porjeto_escola 3/porjeto_escola directory

--- porjeto_escola 3/porjeto_escola/src/main/java/br/com/fatecmaua/projeto_escola/repositories/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fatecmaua.projeto_escola.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.fatecmaua.projeto_escola.models.Professor;
-
-
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-
-}
